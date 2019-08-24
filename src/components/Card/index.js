0ecdccad002c3348b49485cc4264a266a9d399e0
Lucas Teixeira <lucas.at.negocios@gmail.com>
2019-08-24 16:05:00 -0300

    Configuraçoes iniciais do react-dnd

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 
+import { useDrag } from "react-dnd";
+
 import { Container, Label } from "./style";
 
 export default function Card({ data }) {
+  const [{ isDragging }, dragRef] = useDrag({
+    item: { type: "CARD" },
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
+    })
+  });
+
   return (
-    <Container>
+    <Container isDragging={isDragging} ref={dragRef}>
       <header>
         {data.labels.map(label => (
           <Label key={label} color={label} />
